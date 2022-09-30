b0df90e604674b5794772e274e72fd079cf5c1d3
ImDaeseong <cs93059@gmail.com>
2022-09-30 11:15:15 +0900

    DaeseongDjango

--- web1_board_rest_framework/board/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import board_content
-
-
-class board_contentForm(forms.ModelForm):
-    class Meta:
-        model = board_content
-        fields = ['sTitle', 'sContent', 'iImage']
